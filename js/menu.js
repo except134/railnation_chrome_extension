2583631774dfba3075dcb178c40d5af0d5e678ca
Andrey Shpak <ashpak@ashpak.ru>
2016-12-07 10:44:50 +0300

    Correct menu.ts

--- js/menu.js
+++ js/menu.js
@@ -1,2 +1,9 @@
 $(document).ready(function () {
+    // Load menu bar
+    $(".nav a").on("click", function () {
+        $(".nav").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+        $(".extension_part").addClass("hidden");
+        $("#show_" + this.id).removeClass("hidden");
+    });
 });
